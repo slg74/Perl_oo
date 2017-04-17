9b6062b811c58b280204f4841d501833259e0c42
Scott Gillespie <scott.layne.gillespie@gmail.com>
2017-04-16 21:12:01 -0500

    update

--- o.pl
+++ o.pl
@@ -4,22 +4,61 @@ use strict;
 use warnings;
 use feature 'say';
 use feature 'switch';
-use Animal::Cat;
 
-my $whiskers = new Animal::Cat("whiskers", "Scott"); 
-say $whiskers->getName();
+sub animal_actions {
+	use Animal::Cat;
 
-$whiskers->setName("Whiskers");
+	my $whiskers = new Animal::Cat("whiskers", "Scott"); 
+	say $whiskers->getName();
 
-say $whiskers->getName();
-say $whiskers->getSound(); 
+	$whiskers->setName("Whiskers");
 
-use Animal::Lion;
+	say $whiskers->getName();
+	say $whiskers->getSound(); 
 
-my $king = new Animal::Lion("King", "No Owner"); 
+	use Animal::Lion;
 
-say $king->getSound(); 
-say $king->getOwner(); 
+	my $king = new Animal::Lion("King", "No Owner"); 
 
-say $whiskers->getOwner(); 
+	say $king->getSound(); 
+	say $king->getOwner(); 
 
+	say $whiskers->getOwner(); 
+}
+
+animal_actions(); 
+
+sub automobile_actions {
+	use Automobile::Car;
+
+	my $gt = new Automobile::Car("GT40", "Scott", "Blue", "VRooM");
+
+	say $gt->getModel();
+	say $gt->getColor(); 
+	say $gt->getSound(); 
+
+	my $cm = $gt->getModel(); 
+	my $cc = $gt->getColor();
+	my $cs = $gt->getSound(); 
+	my $co = $gt->getOwner(); 
+
+	say "$cm $cc $cs $co"; 
+
+	use Automobile::Truck;
+
+	my $t = new Automobile::Truck("RAM", "Scott", "Black", "RAGARAGARAGA");
+
+	say $t->getModel();
+	say $t->getColor(); 
+	say $t->getSound(); 
+
+	my $m = $t->getModel(); 
+	my $c = $t->getColor();
+	my $s = $t->getSound(); 
+	my $o = $t->getOwner(); 
+
+	say "$m $c $s $o"; 
+
+}
+
+automobile_actions(); 
